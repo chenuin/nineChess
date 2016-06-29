cac8e25aaff9d418895704d75fb06f7e44f62084
yuyin <yuyin0036@gmail.com>
2016-06-30 04:06:53 +0800

    combine ver.1

--- NineServer.py
+++ NineServer.py
@@ -32,7 +32,18 @@ class Chessboard:
 	def is_my_turn(self, my_piece):
 		return my_piece == self.piece
 
+	def choose_piece(self, r, c):
+		if self.grid[r][c]==self.piece:
+			self.grid[r][c] = 'r'
+			return True
+		return False
+		
 	def set_piece(self, r, c):
+		for i in range(3):
+			for j in range(8):
+				if self.grid[i][j] == 'r':
+					self.grid[i][j] = '.'
+	
 		if self.can_set_piece(r, c):
 			self.grid[r][c] = self.piece
 			
@@ -57,11 +68,19 @@ class Chessboard:
 
 		return self.grid[r][c] == '.'
 
+	def take_other_piece(self, r, c):
+		self.grid[r][c] = '.'
+		return True
+		
+	def printboard(self):
+		for j in range(8):
+			print (self.grid[0][j] + "\t" + self.grid[1][j]  + "\t" + self.grid[2][j])
+		
+		
 def receive_message(client_socket):
 	"""接收玩家傳送的訊息"""
 	while True:
 		message = client_socket.recv(1024)
-
 		if message == b'':
 			break
 
@@ -83,12 +102,13 @@ def main():
 
 	# 初始化 server_socket
 	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server_socket.bind(('127.0.0.1', 22004))
+	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	server_socket.bind(('127.0.0.1', 26100))
 	server_socket.listen(2)
 
 	# 等待兩位玩家連線
 	# 預設是0號玩家執黑子，1號玩家執白子
-	
+
 	while len(players) < 2:
 		(client_socket, address) = server_socket.accept()
 
@@ -129,6 +149,7 @@ def main():
 					players[0].send(job)
 					players[1].send(job)
 					board.turn_piece()
+					board.printboard()
 		
 		elif job[0] == ord('4'):
 			pos_msg = job[1:].decode("utf-8")
@@ -140,8 +161,22 @@ def main():
 				if board.clear_piece(r, c):	
 					players[0].send(job)
 					players[1].send(job)
-
-		
+					board.take_other_piece(r, c)
+					board.printboard()
+					
+		elif job[0] == ord('5'):
+			pos_msg = job[1:].decode("utf-8")
+			if len(pos_msg.split(',')) == 2:
+				r,c = pos_msg.split(',')
+				r = int(r)
+				c = int(c)
+				print("clear_piece:{}".format(board.clear_piece(r, c)))
+				if board.clear_piece(r, c):	
+					players[0].send(job)
+					players[1].send(job)
+					board.choose_piece(r, c)
+					board.printboard()
+			
 if __name__ == '__main__':
 	main()
 
